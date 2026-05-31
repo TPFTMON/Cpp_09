8295c1eccbeef858de8bdcd6b4fdb34d93705f7f
TPFTMON <tpftmon@proton.me>
2026-05-31 20:31:26 +0200

    ex00: trim utility was implemented. + small fixes

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -4,36 +4,25 @@
 //                         ORTHODOX BASE
 // ================================================================
 
-BitcoinExchange::BitcoinExchange(){
+BitcoinExchange::BitcoinExchange() {}
 
-    std::cout << DEF_CONSTR_MSG << BITCOINEXCHANGE_MSG;
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &other){
 
+    *this = other;
 }
 
-BitcoinExchange::BitcoinExchange(const BitcoinExchange &to_copy){
-
-    std::cout << COPY_CONSTR_MSG << BITCOINEXCHANGE_MSG;
-    // ... copying
-
-}
 
 void BitcoinExchange::swap(BitcoinExchange &other){
-
     this->_database.swap(other._database);
 }
 
-
 BitcoinExchange& BitcoinExchange::operator=(BitcoinExchange other){
-
     swap(other);
     return (*this);
 };
 
-BitcoinExchange::~BitcoinExchange(){
 
-    std::cout << DESTR_MSG << BITCOINEXCHANGE_MSG;
-
-}
+BitcoinExchange::~BitcoinExchange() {}
 
 
 
@@ -41,8 +30,23 @@ BitcoinExchange::~BitcoinExchange(){
 //                OTHER BITCOINEXCHANGE MEMBER FUNCTIONS
 // ================================================================
 
-// ... other members
+std::string BitcoinExchange::_trim(const std::string& str) const{
 
+    size_t first = str.find_first_not_of(" \t\n\r\f\v");
+    if (first == std::string::npos)
+        return ("");
+
+    size_t last = str.find_last_not_of(" \t\n\r\f\v");
+    return (str.substr(first, (last - first + 1)));
+}
+
+bool BitcoinExchange::_parseValue(const std::string& valStr, float& value, bool isInput) const{
+
+}
+
+void BitcoinExchange::loadDatabase(const std::string& dbPath){
+
+}
 
 
 // ================================================================
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -5,19 +5,10 @@
 #include <iostream>
 #include <string>
 #include <map>
-// #include <fstream>
-// #include <sstream>
-// #include <cstdlib>
-// #include <cctype>
-
-
-// Messages:
-# define BITCOINEXCHANGE_MSG "\e[0;32mBitcoinExchange\n\e[0m"
-
-# define DEF_CONSTR_MSG "\e[0;33mDefault Constructor\e[0m called of "
-# define COPY_CONSTR_MSG "\e[0;33mCopy Constructor\e[0m called of "
-# define COPY_ASSIGN_OP_MSG "\e[0;35mCopy assignment operator\e[0m called of "
-# define DESTR_MSG "\e[0;31mDestructor\e[0m called of "
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <cctype>
 
 
 // Classes:
